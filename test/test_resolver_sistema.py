4ce84537da274673cd8171b9fc47ee4de2a2b92c
Clyzzer <businesschris245@gmail.com>
2024-11-15 22:05:54 -0400

    haciendo commits

--- test/test_resolver_sistema.py
+++ test/test_resolver_sistema.py
@@ -4,9 +4,6 @@ import pytest
 import numpy as np
 from src.Resolver_Sistema import resolver_sistema  # Importar la función desde el módulo correcto
 
-
-
-
 def test_resolver_sistema():
     # Caso 1: Sistema resoluble
     data1 = pd.DataFrame({
